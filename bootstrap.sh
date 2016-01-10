c3aa989f50333c1a9b12f715c2832f9d36bab693
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-10 15:12:53 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,16 @@ sudo apt-get install -y  gparted testdisk git
 echo "GIT setup:"
 git config --global user.email "colinas.maoling@t-online.de"
 git config --global user.name "Colinas Maoling"
-if [ -d DS81 ]; then
+DEFAULT_REPOSITORY="DS81"
+if [ -d  "$DEFAULT_REPOSITORY"]; then
   cd DS81 
   git pull
   cd ..
 else
-  git clone https://github.com/cmaoling/DS81.git
+  git clone https://github.com/cmaoling/$DEFAULT_REPOSITORY.git
 fi
 #https://raw.githubusercontent.com/cmaoling/DS81/master/bootstrap.sh
+echo "Git repro set up."
+echo "Change User Desktop. Current file: "`gsettings get org.gnome.desktop.background picture-uri`
+gsettings set org.gnome.desktop.background picture-uri file://`pwd`/$DEFAULT_REPOSITORY/Bilder/desktop.background
+echo "   => "`gsettings get org.gnome.desktop.background picture-uri`
