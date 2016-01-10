aebb0a09e5f97a6e9ebc4700869dd5dd8c9392a2
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-10 14:57:02 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 
 sudo apt-get update
 sudo apt-get install -y  gparted testdisk git
-git clone 
+echo "GIT setup:"
+git config --global user.email "colinas.maoling@t-online.de"
+git config --global user.name "Colinas Maoling"
+git clone https://github.com/cmaoling/DS81.git
 #https://raw.githubusercontent.com/cmaoling/DS81/master/bootstrap.sh
