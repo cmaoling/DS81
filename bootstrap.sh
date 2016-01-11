4304c808f5d5638d723d4191e947033ac73b6f99
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-11 21:41:14 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,3 +19,4 @@ gsettings set org.gnome.desktop.background picture-uri file://`pwd`/$DEFAULT_REP
 echo "   => "`gsettings get org.gnome.desktop.background picture-uri`
 echo "Enable crypt:"
 sudo apt-get install -y cryptsetup
+export KEYSTICK=`cat keystick`
