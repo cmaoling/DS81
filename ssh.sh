33b378ac9a501f8d115087ea61165fb7b35c9321
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-12 21:35:15 +0100

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,1 +1,22 @@
-
+DEFAULT=secret
+ALGORITHM=rsa
+FILE="/media/$USER/$KEYSTICK/nutshell.$DEFAULT"
+if [ -e $FILE ]; then
+  echo "Found special keystick w/ encrypted repository under $FILE"
+else
+  echo "Please insert keystick w/ encrypted repository and start script again. (FILE=$FILE)"
+  exit
+fi
+./tools/cryptFile $DEFAULT
+if [ -d ./$DEFAULT ]; then
+   if [ -e ~/.ssh/id_$ALGORITHM ]; then
+      echo "Saving existing $ALGORITHM key under ~/.ssh/id_$ALGORITHM.$$"
+      cp ~/.ssh/id_$ALGORITHM ~/.ssh/id_$ALGORITHM.$$
+      cp ~/.ssh/id_$ALGORITHM.pub ~/.ssh/id_$ALGORITHM.$$.pub
+   fi
+   cp ./$DEFAULT/id_$ALGORITHM ~/ssh/
+   cp ./$DEFAULT/id_$ALGORITHM.pub ~/ssh/
+else
+   echo "Unable to find ./$DEFAULT. Assuming cryptFile did not complete sucessfully"
+   exit
+fi
