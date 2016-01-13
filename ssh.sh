d1d16dd08478ea359cace23ba6b90167ceae3be2
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-13 21:46:51 +0100

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -17,12 +17,17 @@ if [ -d ./$DEFAULT ]; then
       cp $SSHDIR/id_$ALGORITHM $SSHDIR/id_$ALGORITHM.$$
       cp $SSHDIR/id_$ALGORITHM.pub $SSHDIR/id_$ALGORITHM.$$.pub
    fi
-   echo "cp ./$DEFAULT/id_$ALGORITHM $SSHDIR/"
+   echo "copying identity from ./$DEFAULT/id_$ALGORITHM to $SSHDIR/"
    cp "./$DEFAULT/id_$ALGORITHM" "$SSHDIR/"
    cp "./$DEFAULT/id_$ALGORITHM.pub" "$SSHDIR/"
+   echo "generate known_hosts file:"
    touch -m "$SSHDIR/known_hosts"
    cat ./$DEFAULT/known_hosts/* >> "$SSHDIR/known_hosts"
+   echo "known_hosts file generated with "`wc -l $SSHDIR/known_hosts`" entries"
+   echo "register identity"
+   ssh-add $SSHDIR/id_$ALGORITHM
 else
    echo "Unable to find ./$DEFAULT. Assuming cryptFile did not complete sucessfully"
    exit
 fi
+echo "setup complete"
