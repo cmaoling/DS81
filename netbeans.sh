e06979794888a7b883afe79332498f82287d22d7
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-19 21:31:14 +0100

    Update netbeans.sh

--- netbeans.sh
+++ netbeans.sh
@@ -3,9 +3,14 @@ VERSION=8.0.1
 DISPLAY=:0
 TOOL=netbeans
 echo $PATH
+local tmpPath=PATH
 TOOLPATH=/tmp/docker-netbeans.$$
+mkdir $TOOLPATH
 PATH=$PATH:$TOOLPATH
 echo $PATH
 docker pull fgrehm/netbeans:v$VERSION
 L=$TOOLPATH/$TOOL && curl -sL https://github.com/fgrehm/docker-netbeans/raw/master/netbeans > $L && chmod +x $L
 docker run -ti --rm -e DISPLAY=$DISPLAY   -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`/.netbeans-docker:/home/developer/.netbeans  -v `pwd`:/workspace fgrehm/netbeans:v$VERSION
+PATH=$tmpPath
+rm -rf $TOOLPATH
+echo $PATH
