778c4ea4d0c8faf13fd1b18eb1e520e0e149ed38
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-23 15:28:28 +0100

    Update netbeans.sh

--- netbeans.sh
+++ netbeans.sh
@@ -8,7 +8,10 @@ mkdir $TOOLPATH
 PATH=$PATH:$TOOLPATH
 docker pull fgrehm/netbeans:v$VERSION
 L=$TOOLPATH/$TOOL && curl -sL https://github.com/fgrehm/docker-netbeans/raw/master/netbeans > $L && chmod +x $L
-docker run -ti --rm -e DISPLAY=$DISPLAY   -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`/.netbeans-docker:/home/developer/.netbeans  -v `pwd`:/workspace fgrehm/netbeans:v$VERSION
+docker run -ti --rm -e DISPLAY=$DISPLAY   -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v `pwd`/.netbeans-docker:/home/developer/.netbeans  -v `pwd`:/workspace fgrehm/netbeans:v$VERSION
+export containerId='docker ps -l -q'
+xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerId`
+docker start $containerId
 ls -la $TOOLPATH
 /bin/rm -rf $TOOLPATH 
-http://wiki.ros.org/docker/Tutorials/GUI
+#http://wiki.ros.org/docker/Tutorials/GUI
