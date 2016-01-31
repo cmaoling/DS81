c9f5f6f4ce986d2de76585640991667ee7385eb9
Colinas Maoling <colinas.maoling@t-online.de>
2016-01-31 11:56:59 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install -y  gparted testdisk git
 echo "GIT setup:"
-git config --global user.email "colinas.maoling@t-online.de"
+git config --global user.email "c.m@t-o.de"
 git config --global user.name "Colinas Maoling"
 DEFAULT_REPOSITORY="DS81"
 if [ -d  "$DEFAULT_REPOSITORY" ]; then
